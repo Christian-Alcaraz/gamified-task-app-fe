9a430e0ea6aa5073227b86d0924dfb8a9e8c3785
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-07-26 20:33:31 +0800

    build: installed angular v20, prettier, tailwindcss, eslint, ng-icons dependencies

---
+++ src/app/app.config.ts
@@ -0,0 +1,34 @@
+import {
+  ApplicationConfig,
+  inject,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import {
+  provideNgIconLoader,
+  provideNgIconsConfig,
+  withCaching,
+  withContentSecurityPolicy,
+} from '@ng-icons/core';
+import { routes } from './app.routes';
+import { HttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideNgIconsConfig(
+      {
+        size: '1.5em',
+      },
+      withContentSecurityPolicy(),
+    ),
+    provideNgIconLoader((name) => {
+      const http = inject(HttpClient);
+      return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
+    }, withCaching()),
+  ],
+};
