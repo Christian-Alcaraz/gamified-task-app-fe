7e11794f94cb2f96d5b40be6d174c8decba476be
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-05 01:58:26 +0800

    refactor: added index.ts in shared/components

---
+++ src/app/shared/components/inputs/index.ts
@@ -0,0 +1,4 @@
+export * from './input-error/input-error.component';
+export * from './password-field/password-field.component';
+export * from './select-field/select-field.component';
+export * from './text-field/text-field.component';
