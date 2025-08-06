a9b3b989cd0c8813fd7f844206934ea13ca9804f
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-06 21:05:25 +0800

    fix: NgIcon cannot be used outside app.component hierarchy/tree; This error was found while trying to use NgIcon in Angular Cdk Dialog where the component lies outside the app html element tree. We instead provideIcons in bootstrapApplication

--- src/app/app.component.config.ts
+++ src/app/app.component.config.ts
@@ -1,34 +1,48 @@
 import {
   ApplicationConfig,
-  inject,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import {
-  provideNgIconLoader,
   provideNgIconsConfig,
-  withCaching,
   withContentSecurityPolicy,
 } from '@ng-icons/core';
 import { routes } from './app.component.routes';
-import { HttpClient } from '@angular/common/http';
+
+import { provideIcons } from '@ng-icons/core';
+import * as heroIconsMicro from '@ng-icons/heroicons/micro';
+import * as heroIconsMini from '@ng-icons/heroicons/mini';
+import * as heroIconsOutline from '@ng-icons/heroicons/outline';
+import * as heroIconsSolid from '@ng-icons/heroicons/solid';
+import * as iconSaxBold from '@ng-icons/iconsax/bold';
+import * as iconSaxBulk from '@ng-icons/iconsax/bulk';
+import * as iconSaxOutline from '@ng-icons/iconsax/outline';
+
+const compiledIcons = {
+  ...iconSaxBold,
+  ...iconSaxOutline,
+  ...iconSaxBulk,
+  ...heroIconsMicro,
+  ...heroIconsMini,
+  ...heroIconsOutline,
+  ...heroIconsSolid,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideIcons(compiledIcons),
     provideNgIconsConfig(
       {
         size: '1.5em',
       },
       withContentSecurityPolicy(),
     ),
-    provideNgIconLoader((name) => {
-      const http = inject(HttpClient);
-      return http.get(`/assets/icons/${name}.svg`, { responseType: 'text' });
-    }, withCaching()),
   ],
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeAwareComponent } from '@core/classes/theme-aware-component.class';
 import { Const } from '@core/constants';
-import { provideIcons } from '@ng-icons/core';
-import * as heroIconsMicro from '@ng-icons/heroicons/micro';
-import * as heroIconsMini from '@ng-icons/heroicons/mini';
-import * as heroIconsOutline from '@ng-icons/heroicons/outline';
-import * as heroIconsSolid from '@ng-icons/heroicons/solid';
-import * as iconSaxBold from '@ng-icons/iconsax/bold';
-import * as iconSaxBulk from '@ng-icons/iconsax/bulk';
-import * as iconSaxOutline from '@ng-icons/iconsax/outline';
-
-const compiledIcons = {
-  ...iconSaxBold,
-  ...iconSaxOutline,
-  ...iconSaxBulk,
-  ...heroIconsMicro,
-  ...heroIconsMini,
-  ...heroIconsOutline,
-  ...heroIconsSolid,
-};
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  viewProviders: [provideIcons(compiledIcons)],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
