3ac367bc59285a95beb97f2d5298f490f6e7128a
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-06 22:38:36 +0800

    feat: added conditional css in DialogContentDirective based on DialogPosition

--- src/app/shared/components/dialog/dialog-content.directive.ts
+++ src/app/shared/components/dialog/dialog-content.directive.ts
@@ -1,5 +1,8 @@
-import { Directive } from '@angular/core';
+import { Directive, inject } from '@angular/core';
+import { BaseDialogPosition } from './base-dialog.class';
+import { BASE_DIALOG_TOKEN } from './dialog.provider';
 
+const SPACE = ' ';
 @Directive({
   selector: '[appDialogContent]',
   standalone: true,
@@ -8,6 +11,22 @@ import { Directive } from '@angular/core';
   },
 })
 export class DialogContentDirective {
-  class =
-    'text-card-foreground text-sm flex flex-col relative w-full flex-1 px-6 py-2';
+  private readonly _baseDialog = inject(BASE_DIALOG_TOKEN, { optional: true });
+
+  class!: string;
+
+  constructor() {
+    const position = this._baseDialog?.position;
+    let css =
+      'text-card-foreground text-sm flex flex-col relative w-full flex-1 px-6 py-2' +
+      SPACE;
+
+    switch (position) {
+      case BaseDialogPosition.Center:
+        css += 'mb-4';
+        break;
+    }
+
+    this.class = css;
+  }
 }
