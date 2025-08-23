1b37c18ac49a292932ec063548fb998b560aab54
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-23 17:07:30 +0800

    chore: cleanup

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -26,17 +26,7 @@ export interface MenuItem {
     </main>
   `,
   styles: `
-    /* These styles rely on CSS variables that can be imported from ng-primitives/example-theme/index.css in your global styles */
     :host {
-      // position: fixed;
-      // display: flex;
-      // flex-direction: column;
-      // width: max-content;
-      // background: var(--ngp-background);
-      // border: 1px solid var(--ngp-border);
-      // box-shadow: var(--ngp-shadow);
-      // border-radius: 8px;
-      // padding: 4px;
       animation: menu-show 150ms var(--burst-in-slow-out);
     }
 
