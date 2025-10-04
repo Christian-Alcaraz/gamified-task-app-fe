e9a0aa361ce538d22e71bfef3332a052a443d746
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-10-04 20:21:34 +0800

    feat: updated table, input

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  input,
+  OnInit,
+  output,
+  Signal,
+  signal,
+} from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import {
   createAngularTable,
@@ -11,20 +18,12 @@ import {
   ColumnFiltersState,
   getCoreRowModel,
   getSortedRowModel,
+  Table,
 } from '@tanstack/table-core';
 import { NgpButton } from 'ng-primitives/button';
 import { NgpMenuTrigger } from 'ng-primitives/menu';
 import { environment } from 'src/environments/environment';
 import { TableDropMenuComponent } from './table-drop-menu/table-drop-menu.component';
-
-interface Person {
-  firstName: string;
-  lastName: string;
-  age: number;
-  visits: number;
-  status?: string;
-  progress: number;
-}
 
 @Component({
   selector: 'app-table',
@@ -40,121 +39,60 @@ interface Person {
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent {
-  defaultData: Person[] = [
-    {
-      firstName: 'tanner',
-      lastName: 'linsley',
-      age: 24,
-      visits: 100,
-      status: 'In Relationship',
-      progress: 50,
-    },
-    {
-      firstName: 'tandy',
-      lastName: 'miller',
-      age: 40,
-      visits: 40,
-      progress: 80,
-    },
-    {
-      firstName: 'joe',
-      lastName: 'dirte',
-      age: 45,
-      visits: 20,
-      status: 'Complicated',
-      progress: 10,
-    },
-  ];
+export class TableComponent<TData> implements OnInit {
+  title = input.required<string>();
+  data = input.required<TData[]>();
+  columns = input.required<ColumnDef<TData>[]>();
+  addItem = output<void>();
 
-  defaultColumns: ColumnDef<Person>[] = [
-    {
-      accessorKey: 'firstName',
-      header: 'First Name',
-      sortingFn: 'alphanumeric',
-    },
-    {
-      accessorKey: 'lastName',
-      header: `Last Name`,
-      sortingFn: 'alphanumeric',
-    },
-    {
-      accessorKey: 'age',
-      header: 'Age',
-      sortingFn: 'alphanumeric',
-    },
-    {
-      accessorKey: 'visits',
-      header: `Visits`,
-      sortingFn: 'alphanumeric',
-    },
-    {
-      accessorKey: 'status',
-      header: 'Status',
-      sortingFn: 'alphanumeric',
-    },
-    {
-      accessorKey: 'progress',
-      header: 'Profile Progress',
-      enableSorting: false,
-    },
-    // {
-    //   id: 'actions',
-    //   accessorFn: (row) => flexRenderComponent(),
-    //   header: 'Actions',
-    // },
-  ];
+  readonly columnFilters = signal<ColumnFiltersState>([]);
 
-  /**
+  table!: Table<TData> & Signal<Table<TData>>;
+  columnCount!: number;
+  viewMenuItems!: Record<string, string | Column<TData>>[];
+
+  ngOnInit(): void {
+    this.table = createAngularTable(() => ({
+      data: this.data(),
+      columns: this.columns(),
+      getCoreRowModel: getCoreRowModel(),
+      getSortedRowModel: getSortedRowModel(),
+      state: {
+        columnFilters: this.columnFilters(),
+      },
+      onColumnFiltersChange: (updater) => {
+        if (updater instanceof Function) {
+          return this.columnFilters.update(updater);
+        }
+        return this.columnFilters.set(updater);
+      },
+      debugTable: !environment.PRODUCTION,
+    }));
+
+    this.columnCount = this.table.getAllColumns().length;
+    this.viewMenuItems = this.table.getAllLeafColumns().map((column) => ({
+      label: column.columnDef.header as string,
+      column,
+    }));
+  }
+
+  viewMenuItemSelected(column: Column<TData>) {
+    column.toggleVisibility();
+  }
+
+  sortColumnByHeader(column: Column<TData>) {
+    if (!column.getCanSort()) return;
+    column.toggleSorting();
+  }
+}
+/**
    *     accessorFn: row => row.lastName,
     id: 'lastName',
     cell: info => `<i>${info.getValue<string>()}</i>`,
    */
 
-  data = signal<Person[]>(this.defaultData);
-
-  readonly columnFilters = signal<ColumnFiltersState>([]);
-
-  table = createAngularTable(() => ({
-    data: this.data(),
-    columns: this.defaultColumns,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    state: {
-      columnFilters: this.columnFilters(),
-    },
-    onColumnFiltersChange: (updater) => {
-      if (updater instanceof Function) {
-        return this.columnFilters.update(updater);
-      }
-      return this.columnFilters.set(updater);
-    },
-    debugTable: !environment.PRODUCTION,
-  }));
-
-  columnCount = this.table.getAllColumns().length;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  viewMenuItems!: any;
-
-  rerender() {
-    this.data.set([...this.defaultData.sort(() => -1)]);
-  }
-
-  constructor() {
-    this.viewMenuItems = this.table.getAllLeafColumns().map((column) => ({
-      label: column.columnDef.header,
-      column,
-    }));
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  viewMenuItemSelected(column: Column<any>) {
-    column.toggleVisibility();
-  }
-
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  sortColumnByHeader(column: Column<any>) {
-    if (!column.getCanSort()) return;
-    column.toggleSorting();
-  }
-}
+// {
+//   id: 'actions',
+//   accessorFn: (row) => flexRenderComponent(),
+//   header: 'Actions',
+// },
